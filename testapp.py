d75a066f40e7482a570f8cdacbc3eeffefeea378
tweath <tetzmimu@yahoo.com>
2016-04-11 01:06:49 -0500

    Modified testapp to handle larger inputs

--- testapp.py
+++ testapp.py
@@ -1,18 +1,41 @@
 # File: 	testapp.py
 # Author:	Bonnie Jan
-# Date:		4/6/2016
+# Date:		4/6/2016, modified 4/10/16
 # Program:	testapp
 #
 # Retrieve line from user terminated by Enter and save to file data.txt.
 # Each entry is appended as a new line at the end of the file.
+#
+# PRE: Text files for input and output are in UTF-8 format, so conversion 
+#      isn't needed for input and output to be in the same format. Input 
+#      is expected to be passed from a file using
+#
+#      cat FILE | python testapp.py
+#
+#      at the command line, where FILE is replaced by the name of the
+#      input file. File is formatted with each line ended by '\n'.
+# POST: data.txt contains input from FILE passed through stdin. Has been 
+#       tested to work with 40 MB file.
 
-# Open file for appending and close after
-with open('data.txt', 'a') as f:
 
-	# Prompt for input
-	str = input('Please enter text to save to data.txt: \n')
+import sys
 
-	# Save data to file
-	f.write(str + "\n")
+# Function to open file for appending and close after
+def write_stdin_to_file(filename):
+	
+	# Open file
+	with open(filename, 'a') as f:
+	
+		# Take input from stdin
+		for line in sys.stdin:
+
+			# Save data to file
+			f.write(str(line))
+			
+if __name__ == "__main__":
+	write_stdin_to_file('data.txt')
+
+
+
 
 
